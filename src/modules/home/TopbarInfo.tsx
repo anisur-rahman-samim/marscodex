b6d2877ac7338123a5bd97d935a402646bb2bf52
dipujafar <dipujafarmr@gmail.com.>
2024-11-08 17:34:29 +0600

    set up and create topbar componetns

---
+++ src/modules/home/TopbarInfo.tsx
@@ -0,0 +1,12 @@
+const TopbarInfo = () => {
+  return (
+    <div className="bg-">
+      <div className="flex-center">
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default TopbarInfo;
