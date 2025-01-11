35dbd5c922cd01e2edd33b9644e63a98be95a890
dipujafar <dipujafarmr@gmail.com>
2025-01-11 23:26:16 +0600

    work on footer

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="flex justify-center items-center">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center items-center">
       <h1>footer</h1>
     </div>
   );
